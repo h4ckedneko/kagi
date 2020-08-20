a66a708b78cfe458ee0e3d088d2e5117f2377da2
Lyntor Paul Figueroa <h4ckedneko@tfwno.gf>
2020-08-20 11:44:49 +0800

    Improve cli example

--- examples/cli/main.go
+++ examples/cli/main.go
@@ -18,7 +18,7 @@ var (
 	print bool
 )
 
-func init() {
+func main() {
 	flag.StringVar(&env, "env", ".env", "Set the env file to use.")
 	flag.IntVar(&size, "size", 32, "Set the size of key to generate.")
 	flag.BoolVar(&print, "print", false, "Show the key instead of writing to file.")
@@ -27,30 +27,54 @@ func init() {
 	if err := dotenv.Load(env); err != nil {
 		log.Fatalln(err)
 	}
+
+	var err error
+	defer func() {
+		if err != nil {
+			log.Fatalln(err)
+		}
+	}()
+
+	switch flag.Arg(0) {
+	case "generate":
+		err = runGenerate()
+	case "decode":
+		err = runDecode()
+	default:
+		err = fmt.Errorf("unknown command")
+	}
 }
 
-func main() {
+func runGenerate() error {
 	key := kagi.New(size)
 	if print {
 		fmt.Println(key)
-		return
+		return nil
 	}
 
 	ck := "APP_KEY"
 	cv := regexp.QuoteMeta(os.Getenv(ck))
 
+	// Replace the application from env file.
 	r, err := regexp.Compile(ck + "=" + cv)
 	if err != nil {
-		log.Fatalln(err)
+		return err
 	}
 	b, err := ioutil.ReadFile(env)
 	if err != nil {
-		log.Fatalln(err)
+		return err
 	}
 	br := r.ReplaceAll(b, []byte(ck+"="+key))
 	if err := ioutil.WriteFile(env, br, 0644); err != nil {
-		log.Fatalln(err)
+		return err
 	}
 
 	fmt.Println("Application key was successfully set.")
+	return nil
+}
+
+func runDecode() error {
+	keyd := kagi.Decode(os.Getenv("APP_KEY"))
+	fmt.Println(string(keyd))
+	return nil
 }
